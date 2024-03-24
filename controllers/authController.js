a8da85ae5f3db08b7493d9ee94109125b1deb041
Shpat Spahiu <shpat.spahiu@proton.me>
2024-03-24 01:32:04 +0100

    Init new routes and controllers for auth

---
+++ controllers/authController.js
@@ -0,0 +1,11 @@
+import User from '../models/userModel.js'
+
+export const loginUser = () => {
+
+}
+
+export const registerUser = () => { }
+
+export const confirmRegister = () =>{}
+
+
--- routes/auth.js
+++ routes/auth.js
@@ -1,4 +1,9 @@
 import express from 'express'
 const router = express.Router()
+import { loginUser, registerUser, confirmRegister } from '../controllers/authController.js'
+
+router.post('/email/login', loginUser)
+router.post('/email/register', registerUser)
+router.post('/email/confirm', confirmRegister)
 
 export default router
