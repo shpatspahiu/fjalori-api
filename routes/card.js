23e10096f8094a6847fc30375aaeb0e5e323ea56
Shpat Spahiu <shpat.spahiu@proton.me>
2024-03-24 01:38:12 +0100

    Define user and card routes

--- routes/card.js
+++ routes/card.js
@@ -1,5 +1,18 @@
 import express from 'express'
 const router = express.Router()
 
+router.get('/') // get all cards
+
+router.get('/:id') // get card by id
+
+router.post('/') // create new card
+
+router.delete('/:id') // delete card by id
+
+router.put('/:id') // update card by id
+
+router.get('/user/:id') // get all cards by user id
+
+
 
 export default router
--- routes/user.js
+++ routes/user.js
@@ -1,4 +1,8 @@
 import express from 'express'
 const router = express.Router()
 
+router.get('/') // get all users
+
+router.get('/:id') // get user by id
+
 export default router
